d437b7fd9970ddec783fe541244d4bbc7eb24b64
Ashley <36495929+myashley@users.noreply.github.com>
2018-05-07 11:14:41 +1000

    Migrate location model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507011216) do
+ActiveRecord::Schema.define(version: 20180507011349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20180507011216) do
     t.string "language_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -60,5 +69,6 @@ ActiveRecord::Schema.define(version: 20180507011216) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "locations", "users"
   add_foreign_key "profiles", "users"
 end
