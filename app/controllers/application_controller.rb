bd49d0ef6614c917524084aaa899467f9bf6a509
Ashley <36495929+myashley@users.noreply.github.com>
2018-05-07 13:18:43 +1000

    Add user signout link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  #before_action :set_time_zone, if: :user_signed_in?
+  
 
 
   # private
