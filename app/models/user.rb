07ee1862dc3d616d113e24cf25b487b3d70b9d7e
Ashley <36495929+myashley@users.noreply.github.com>
2018-05-07 13:12:52 +1000

    Edit profile form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+  after_create :create_profile
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :profile
+  has_one :location
+  has_many :languages
+
+  def create_profile
+    Profile.create!([
+      {
+        user_id: self.id
+      }
+    ])
+  end
+
 end
