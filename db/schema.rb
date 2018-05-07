b26ca612c309b2f0c99a34a4f37dd729739e6b15
Ashley <36495929+myashley@users.noreply.github.com>
2018-05-07 11:20:51 +1000

    Migrate join table language and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507011349) do
+ActiveRecord::Schema.define(version: 20180507011817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20180507011349) do
     t.string "language_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "languages_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "language_id", null: false
+    t.index ["user_id", "language_id"], name: "index_languages_users_on_user_id_and_language_id"
   end
 
   create_table "locations", force: :cascade do |t|
