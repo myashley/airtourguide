cf05353598c4fb4ac82550a276b19414379be9d3
Ashley <36495929+myashley@users.noreply.github.com>
2018-05-09 00:50:38 +1000

    Edit small things

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   validates :user, presence: true
+  validates :user_id, uniqueness: { message: 'can only have one profile.' }
 
   def fullname
     "#{first_name} #{last_name}"
