9efa9e28790a73ada423b1f2384c031fd9fdf226
Ashley <36495929+myashley@users.noreply.github.com>
2018-05-09 21:37:03 +1000

    Add booking authorisation

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,27 @@
+class TourPolicy < ApplicationPolicy
+    def index?
+      false
+    end
+   
+    def create?
+      user.present?
+    end
+
+    def edit?
+        return true if user.present? && user == booking.user
+    end
+   
+    def update?
+      return true if user.present? && user == booking.user
+    end
+   
+    def destroy?
+      return true if user.present? && user == booking.user
+    end
+   
+    private
+   
+      def booking
+        record
+      end
+  end
