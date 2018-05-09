1892b84dcae79dd2cdfa3ab193abd98e79a651ce
Ashley <36495929+myashley@users.noreply.github.com>
2018-05-09 19:17:21 +1000

    Edit user sign in routes and profile link

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,28 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-    def after_sign_up_path_for(user)
-      edit_profile_path(user.profile)
+
+  def create
+    super # this calls Devise::RegistrationsController#create as usual
+    # after creating a new user, create a profile that has
+    # the profile.user_id field set to the user_id of the user jsut created
+    if resource.save
+        @profile = Profile.new
+        @profile.user_id = resource.id
+        @profile.save
     end
+  end
+
+  def destroy
+    super
+    if resource.destroy
+        @profile = Profile.find(user_id: resource.id)
+        @profile.destroy
+    end
+  end
+
+
+    # def after_sign_up_path_for(resource)
+    #   edit_profile_path(user.profile)
+    # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :languages
   resources :profiles
   resources :tours
-  devise_for :users#, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   get '/profile', to: 'profiles#show'
   get '/mytours', to: 'tours#mytours', as: :mytours
