e0405d7eb2ce7d33726eb260119930f7e9c2957c
Ashley <36495929+myashley@users.noreply.github.com>
2018-05-09 18:04:39 +1000

    Trail

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-    # def after_sign_up_path_for(user)
-    #   edit_profile_path(user.profile)
-    # end
+    def after_sign_up_path_for(user)
+      edit_profile_path(user.profile)
+    end
 
 end
